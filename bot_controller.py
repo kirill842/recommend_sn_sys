8ff755fe3780b0db4432c4fe0db92704fe9921aa
Kirill <37930588+kirill842@users.noreply.github.com>
2025-02-24 19:08:17 +0300

    Revert "Signed-off-by: Kirill <37930588+kirill842@users.noreply.github.com>"
    This reverts commit 1330845574ac94d6c1d3bd69df963287d36d4cbf.

--- bot_controller.py
+++ bot_controller.py
@@ -32,70 +32,53 @@ def send_next_image(message):
 # Loading last index to mark
 with open('./next_marking_img_id.pkl', 'rb') as file:
     next_marking_img_id = pickle.load(file)
-    print('Последний засейвленный индекс разметки', next_marking_img_id)
-    # print('Last saved next marking index:', next_marking_img_id)
+    print('Last saved next marking index:', next_marking_img_id)
 
 
 with open('./already_recommended_img_ids.pkl', 'rb') as file:
     already_recommended_img_ids = pickle.load(file)
-    print('Уже зарекомендованные кроссовки:', already_recommended_img_ids)
-    # print('Already recommended sneakers:', already_recommended_img_ids)
+    print('Already recommended sneakers:', already_recommended_img_ids)
 
 
 with open('./is_model_up_to_date.pkl', 'rb') as file:
     is_model_up_to_date = pickle.load(file)
-
-    print('Модель натренирована на новых данных?', is_model_up_to_date)
-    # print('Is the model trained on new data?', is_model_up_to_date)
+    print('Is the model trained on new data?', is_model_up_to_date)
 
 with open('./first_launch.pkl', 'rb') as file:
     first_launch = pickle.load(file)
 
 if first_launch is True:
-    # print('It is first launch, we will scrape', config.NUM_OF_PAGES_TO_SCRAP, 'pag. and fill directories for train and inference scripts')
-    print('Это первый запуск, мы спарсим', config.NUM_OF_PAGES_TO_SCRAP,
-          'стр. и заполним директории для тренировки и инференса нейросети')
+    print('It is first launch, we will scrape', config.NUM_OF_PAGES_TO_SCRAP, 'pag. and fill directories for train and inference scripts')
     web_crawler.scrap_site_and_update_database()
     img_load_and_save.load_images_from_url_and_save(config.DB_CONNECT_LINK, './img_data_scraped_from_urls/all')
-    # print('All images were loaded')
-    print('Все изображения были загружены')
+    print('All images were loaded')
     img_load_and_save.convert_images_to_training_format_and_save(config.DB_CONNECT_LINK)
-    # print('All images were transformed ')
-    print('Все изображения были преобразованы ')
+    print('All images were transformed ')
     with open('./first_launch.pkl', 'wb') as file:
         # A new file will be created
         pickle.dump(False, file)
 else:
-    # print('It is not first launch')
-    print('Это не первый запуск')
+    print('It is not first launch')
 
 bot = telebot.TeleBot(TOKEN)
 
-print('Бот готов, заходи в телеграм')
-# print('Bot is ready, go to telegram')
+print('Bot is ready, go to telegram')
 
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     global is_model_up_to_date
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # item1 = types.KeyboardButton('Data tagging mode')
-    # item2 = types.KeyboardButton('New selection of sneakers')
-    # item3 = types.KeyboardButton('Train the model on new data')
-    item1 = types.KeyboardButton('Разметка')
-    item2 = types.KeyboardButton('Подбери')
-    item3 = types.KeyboardButton('Натренируй')
+    item1 = types.KeyboardButton('Data tagging mode')
+    item2 = types.KeyboardButton('New selection of sneakers')
+    item3 = types.KeyboardButton('Train the model on new data')
     markup.add(item1, item2, item3)
 
-    # bot.send_message(message.chat.id, 'The bot has 2 functions: data markup mode and selection of sneakers based on the users taste', parse_mode='html',
-    #                  reply_markup=markup)
-    bot.send_message(message.chat.id, 'У бота есть три функции: режим разметки, режим подборки кроссовок на основе вкуса и режим тренировки нейросети для коррекции вкуса', parse_mode='html',
+    bot.send_message(message.chat.id, 'The bot has 2 functions: data markup mode and selection of sneakers based on the users taste', parse_mode='html',
                      reply_markup=markup)
 
     if not is_model_up_to_date:
-        # bot.send_message(message.chat.id, 'Perhaps the model is outdated, click "Train the model on new data" if you want to train the model on new data', parse_mode='html',
-        #                  reply_markup=markup)
-        bot.send_message(message.chat.id, 'Модель может стать лучше, нажми "Натренируй", чтобы сделать нейросеть лучше', parse_mode='html',
+        bot.send_message(message.chat.id, 'Perhaps the model is outdated, click "Train the model on new data" if you want to train the model on new data', parse_mode='html',
                          reply_markup=markup)
 
 
@@ -106,26 +89,19 @@ def text(message):
     global is_model_up_to_date
     global already_recommended_img_ids
     if message.chat.type == 'private':  # and message.chat.username == 'kirill_lekanov':
-        # if message.text == 'Data tagging mode' and marking_stage is False:
-        if message.text == 'Разметка' and marking_stage is False:
+        if message.text == 'Data tagging mode' and marking_stage is False:
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            # item1 = types.KeyboardButton('I like it')
-            # item2 = types.KeyboardButton('Naah')
-            # item3 = types.KeyboardButton('Back')
-            item1 = types.KeyboardButton('Мне нравится')
-            item2 = types.KeyboardButton('Не очень')
-            item3 = types.KeyboardButton('Назад')
+            item1 = types.KeyboardButton('I like it')
+            item2 = types.KeyboardButton('Naah')
+            item3 = types.KeyboardButton('Back')
             markup.add(item1, item2, item3)
 
-            # bot.send_message(message.chat.id, 'The bot has switched to markup mode: Click "I like it" if you like the sneakers and "Naah" if you dont like the sneakers',
-            #                  parse_mode='html', reply_markup=markup)
-            bot.send_message(message.chat.id, 'Бот был перевед в режим разметки: нажми "Мне нравится", если тебе нравятся кроссовки и "Не очень", если не нравятся',
+            bot.send_message(message.chat.id, 'The bot has switched to markup mode: Click "I like it" if you like the sneakers and "Naah" if you dont like the sneakers',
                              parse_mode='html', reply_markup=markup)
 
             send_next_image(message)
             marking_stage = True
-        # elif message.text == 'New selection of sneakers' and marking_stage is False:
-        elif message.text == 'Подбери' and marking_stage is False:
+        elif message.text == 'New selection of sneakers' and marking_stage is False:
             column_names = ['img_id_jpg', 'target']
             predictions_loaded = False
             try:
@@ -133,10 +109,7 @@ def text(message):
                 predictions_loaded = True
             except Exception as error:
                 print(error)
-                # bot.send_message(message.chat.id, 'The bot did not find the output of the model, first label the data and train the model', parse_mode='html')
-                bot.send_message(message.chat.id,
-                                 'Перед подборкой сначала разметь данные и натренируй модель',
-                                 parse_mode='html')
+                bot.send_message(message.chat.id, 'The bot did not find the output of the model, first label the data and train the model', parse_mode='html')
                 exit()
             if predictions_loaded:
                 good_output = output[output['target'] == 1]
@@ -170,38 +143,26 @@ def text(message):
                     i += 1
                 with open('./already_recommended_img_ids.pkl', 'wb') as file:
                     pickle.dump(already_recommended_img_ids, file)
-        # elif message.text == 'I like it' and marking_stage is True:
-        elif message.text == 'Мне нравится' and marking_stage is True:
+        elif message.text == 'I like it' and marking_stage is True:
             postgres_connection.mark_image_by_index(1, next_marking_img_id)
             increment_next_marking_img_id()
             send_next_image(message)
-        # elif message.text == 'Train the model on new data' and marking_stage is False and (not is_model_up_to_date):
-        elif message.text == 'Натренируй' and marking_stage is False and (not is_model_up_to_date):
-            # print('Training stage')
-            print('Тренировка...')
-            # bot.send_message(message.chat.id, 'Wait for the bot to finish training',
-            #                  parse_mode='html')
-            bot.send_message(message.chat.id, 'Подожди, пока закончится тренировка',
+        elif message.text == 'Train the model on new data' and marking_stage is False and (not is_model_up_to_date):
+            print('Training stage')
+            bot.send_message(message.chat.id, 'Wait for the bot to finish training',
                              parse_mode='html')
-            return_code = os.system('python pytorch-image-models/train.py ./resized_imgs --train-split train --val-split val --model efficientnet_b0 --pretrained --num-classes 2 --input-size 3 224 224 -b 16 --epochs 10 --no-aug --output ./trained_models')
+            return_code = os.system('python pytorch-image-models/train.py ./resized_imgs --train-split train --val-split val --model efficientnet_b0 --pretrained --num-classes 2 --input-size 3 224 224 -b 16 --epochs 40 --no-aug --output ./trained_models')
             if return_code == 0:
-                # print('Model was trained successfully')
-                print('Модель успешно натренирована')
-                # bot.send_message(message.chat.id, 'Training completed successfully! Now wait for the model to predict recommendations',
-                #                  parse_mode='html')
-                bot.send_message(message.chat.id, 'Тренировка была успешно закончена! Теперь подожди пока модель сделает предсказания',
+                print('Model was trained successfully')
+                bot.send_message(message.chat.id, 'Training completed successfully! Now wait for the model to predict recommendations',
                                  parse_mode='html')
                 with open('./is_model_up_to_date.pkl', 'wb') as file:
                     pickle.dump(True, file)
                 is_model_up_to_date = True
             else:
-                # print('Model wasnt trained, check error above. Possible solution might be to label more data')
-                print('Модель не была натренировка, посмотри лог ошибки. Возможно проблема в том, что слишком мало данных для тренировки - разметь больше данных')
-                # bot.send_message(message.chat.id,
-                #                  'Training was not completed successfully, check the error log',
-                #                  parse_mode='html')
+                print('Model wasnt trained, check error above. Possible solution might be to label more data')
                 bot.send_message(message.chat.id,
-                                 'Тренировка не была успешно закончена, посмотри лог ошибки',
+                                 'Training was not completed successfully, check the error log',
                                  parse_mode='html')
 
             def find_all(name, path):
@@ -213,89 +174,55 @@ def text(message):
 
             all_matches = find_all('model_best.pth.tar', './trained_models')
             if len(all_matches) == 1:
-                # print('Bot didnt find any new trained models. Loading trained model by me')
-                print('Бот не нашел никаких натренированных моделей. Загружаю свою модель')
+                print('Bot didnt find any new trained models. Loading trained model by me')
             path_to_best_model = all_matches[len(all_matches) - 1]
             return_code = os.system('python pytorch-image-models/inference.py ./resized_imgs/not_labeled --model efficientnet_b0 --num-classes 2 --topk 1 --checkpoint ' + path_to_best_model + ' --input-size 3 224 224 -b 32 --interpolation bicubic')
             if return_code == 0:
-                # bot.send_message(message.chat.id,
-                #                  'The model predicted recommendations',
-                #                  parse_mode='html')
-                # print('Model output was saved')
                 bot.send_message(message.chat.id,
-                                 'Модель сделала рекомендации',
+                                 'The model predicted recommendations',
                                  parse_mode='html')
-                print('Выход модели был сохранен')
+                print('Model output was saved')
             else:
-                # bot.send_message(message.chat.id,
-                #                  'The model did not predict recommendations, look at the error log',
-                #                  parse_mode='html')
-                # print('Model didnt send output, check error above')
                 bot.send_message(message.chat.id,
-                                 'Модель не сделала предсказания, смотри лог ошибки',
+                                 'The model did not predict recommendations, look at the error log',
                                  parse_mode='html')
-                print('Модель не сделала предсказания, смотри ошибку выше')
-        # elif message.text == 'Train the model on new data' and marking_stage is False and is_model_up_to_date:
-        elif message.text == 'Натренируй' and marking_stage is False and is_model_up_to_date:
-            # bot.send_message(message.chat.id, 'The model is already trained on the new data',
-            #                  parse_mode='html')
-            bot.send_message(message.chat.id, 'Модель уже натренирована на новых данных',
+                print('Model didnt send output, check error above')
+        elif message.text == 'Train the model on new data' and marking_stage is False and is_model_up_to_date:
+            bot.send_message(message.chat.id, 'The model is already trained on the new data',
                              parse_mode='html')
-        # elif message.text == 'Naah' and marking_stage is True:
-        elif message.text == 'Не очень' and marking_stage is True:
+        elif message.text == 'Naah' and marking_stage is True:
             postgres_connection.mark_image_by_index(0, next_marking_img_id)
             increment_next_marking_img_id()
             send_next_image(message)
-        # elif message.text == 'I like it' and marking_stage is False:
-        elif message.text == 'Мне нравится' and marking_stage is False:
-            # print('Error! Please try later')
-            print('Попробуйте позже')
-        # elif message.text == 'Naah' and marking_stage is False:
-        elif message.text == 'Не очень' and marking_stage is False:
-            # print('Error! Please try later')
-            print('Попробуйте позже')
-        # elif message.text == 'Back' and marking_stage is True:
-        elif message.text == 'Назад' and marking_stage is True:
+        elif message.text == 'I like it' and marking_stage is False:
+            print('Error! Please try later')
+        elif message.text == 'Naah' and marking_stage is False:
+            print('Error! Please try later')
+        elif message.text == 'Back' and marking_stage is True:
             with open('./is_model_up_to_date.pkl', 'wb') as file:
                 pickle.dump(False, file)
             is_model_up_to_date = False
             marking_stage = False
             last_labeled_img_id = next_marking_img_id - 1
-            # bot.send_message(message.chat.id, 'The bot saves new data, depending on the amount of data it may take different time, wait for "Done" from the bot', parse_mode='html')
-            bot.send_message(message.chat.id,
-                             'Бот сохраняет новые данные, в зависимости от количества новых данных этот процесс может занять разное время',
-                             parse_mode='html')
+            bot.send_message(message.chat.id, 'The bot saves new data, depending on the amount of data it may take different time, wait for "Done" from the bot', parse_mode='html')
             img_load_and_save.move_new_labeled_imgs_to_proper_dirs(last_labeled_img_id, not_efficient_but_safe=False)
-            # bot.send_message(message.chat.id,
-            #                  'Done',
-            #                  parse_mode='html')
             bot.send_message(message.chat.id,
-                             'Готово',
+                             'Done',
                              parse_mode='html')
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            # item1 = types.KeyboardButton('Data tagging mode')
-            # item2 = types.KeyboardButton('New selection of sneakers')
-            # item3 = types.KeyboardButton('Train the model on new data')
-            item1 = types.KeyboardButton('Разметка')
-            item2 = types.KeyboardButton('Подбери')
-            item3 = types.KeyboardButton('Натренируй')
+            item1 = types.KeyboardButton('Data tagging mode')
+            item2 = types.KeyboardButton('New selection of sneakers')
+            item3 = types.KeyboardButton('Train the model on new data')
             markup.add(item1, item2, item3)
-            # bot.send_message(message.chat.id, 'The bot has 2 functions: data markup mode and selection of sneakers based on the users taste', parse_mode='html',
-            #                  reply_markup=markup)
-            bot.send_message(message.chat.id, 'У бота есть три функции: режим разметки, режим подборки кроссовок на основе вкуса и режим тренировки нейросети для коррекции вкуса', parse_mode='html',
+            bot.send_message(message.chat.id, 'The bot has 2 functions: data markup mode and selection of sneakers based on the users taste', parse_mode='html',
                              reply_markup=markup)
             if not is_model_up_to_date:
-                # bot.send_message(message.chat.id,
-                #                  'Perhaps the model is outdated, click "Train the model on new data" if you want to train the model on new data',
-                #                  parse_mode='html',
-                #                  reply_markup=markup)
                 bot.send_message(message.chat.id,
-                                 'Модель может стать лучше, нажми "Натренируй", чтобы сделать нейросеть лучше',
+                                 'Perhaps the model is outdated, click "Train the model on new data" if you want to train the model on new data',
                                  parse_mode='html',
                                  reply_markup=markup)
     else:
-        # bot.send_message(message.chat.id, 'Error! Try later')
-        bot.send_message(message.chat.id, 'Попробуй позже')
+        bot.send_message(message.chat.id, 'Error! Try later')
 
 
 bot.polling(none_stop=True)
